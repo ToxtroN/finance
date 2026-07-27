80c8539f08c7d02b465412d5880a82789d994d7b
ToxtroN <akkomlew@gmail.com>
2026-07-27 21:21:51 +0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE = 'finance-tracker-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then((cached) => {
+      return (
+        cached ||
+        fetch(e.request)
+          .then((res) => {
+            const resClone = res.clone();
+            caches.open(CACHE).then((c) => c.put(e.request, resClone));
+            return res;
+          })
+          .catch(() => cached)
+      );
+    })
+  );
+});
